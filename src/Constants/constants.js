0e219ccc1534a4453331903e91cdd3acea1e92ec
rk-rishikesh <rvk.rishikeshkale@gmail.com>
2023-12-24 17:58:13 +0530

    Fixed Constants

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -13,50 +13,7 @@ import {
 	faX,
 } from "@fortawesome/free-solid-svg-icons";
 
-import {
-	avatar,
-	awsIcon,
-	calculator,
-	cssIcon,
-	eslintIcon,
-	figmaIcon,
-	gitIcon,
-	githubIcon,
-	htmlIcon,
-	javaIcon,
-	jsIcon,
-	memoryGame,
-	neoIcon,
-	nodeIcon,
-	pawsitivePrototype,
-	psqlIcon,
-	pyIcon,
-	raspIcon,
-	reactIcon,
-	viteIcon,
-} from "../assets";
-
 library.add(faX, faBars, faWindowRestore, faBagShopping, faDiceD6);
-
-const media = {
-	htmlIcon,
-	cssIcon,
-	jsIcon,
-	reactIcon,
-	awsIcon,
-	javaIcon,
-	gitIcon,
-	githubIcon,
-	psqlIcon,
-	eslintIcon,
-	pyIcon,
-	viteIcon,
-	nodeIcon,
-	raspIcon,
-	neoIcon,
-	figmaIcon,
-	avatar,
-};
 
 const icons = {
 	faBars,
@@ -68,72 +25,6 @@ const icons = {
 	faGithub,
 	faLinkedin,
 	faEnvelope,
-};
-
-const introduction = {
-	text: [
-		"Hello there, I'm so happy you are here! (:",
-
-		"My name is Sara, I'm 21 and a junior computer science student based in Guatemala. I have a strong passion for the technologies related to the data base construction and UX.",
-
-		"Outside of my regular activities, I'm into drawing whenever I have some free time. I'm also a huge dog lover and enjoy playing chess for fun.",
-
-		"I'm continuously thrilled to partner with individuals and teams who wholeheartedly embrace my fervor for crafting extraordinary experiences. Your visit to my portfolio is deeply appreciated !",
-	],
-};
-
-export const navLinks = [
-	{
-		id: "about",
-		title: "About",
-	},
-	{
-		id: "projects",
-		title: "Projects",
-	},
-	{
-		id: "skills",
-		title: "Skills",
-	},
-	{
-		id: "contact",
-		title: "Contact",
-	},
-];
-
-const projects = [
-
-
-
-	{
-		name: "Arasartara's Gallery",
-		description:
-			"A memory game with an art theme implemented with React. Test your memory skills and enjoy beautiful artwork as you match pairs of cards in this engaging game.",
-		image: memoryGame,
-		source_code_link: "https://github.com/bl33h/artMemoryGame",
-		demo_link: "https://arasarmemory.netlify.app/",
-	},
-	{
-		name: "Pawsitive",
-		description:
-			"Explore a user-friendly Figma prototype for an adoption and sterilization awareness website. Experience the sleek design and contribute to a noble cause.",
-		image: pawsitivePrototype,
-		source_code_link: "https://github.com/bl33h/pawsitive",
-		demo_link:
-			"https://www.figma.com/proto/zhK8DMa7uPyQIcHxkemIcx/Pawsitive?type=design&node-id=40-2&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=40%3A2",
-	},
-	{
-		name: "Basic Calculator",
-		description:
-			"A completely functional basic calculator website created with React. It allows you to perform mathematical calculations effortlessly. With testing implemented, the results are accurate.",
-		image: calculator,
-		source_code_link: "https://github.com/bl33h/calculator",
-		demo_link: "https://bl33hscalculator.netlify.app/",
-	},
-];
-
-const memoji = {
-	image: [avatar],
 };
 
 const skills = [
@@ -256,4 +147,4 @@ const markerSvg = `<svg viewBox="-4 0 36 36">
     <circle fill="black" cx="14" cy="14" r="7"></circle>
   </svg>`;
 
-export { icons, introduction, markerSvg, media, memoji, projects, skills };
+export { icons, markerSvg, skills };
